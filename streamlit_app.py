6c99df4818382fa30e517b84ce4fd1f3d3e0e6f9
Dash Desai <iamontheinet@gmail.com>
2025-06-16 08:34:29 -0700

    Updated for 2026

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def dhms_from_seconds(seconds):
 	days, hours = divmod(hours, 24)
 	return (days, hours, minutes, seconds)
 
-data_cloud_summit_24_date = datetime.strptime('2025-06-02 08:00:00', '%Y-%m-%d %H:%M:%S')
+data_cloud_summit_24_date = datetime.strptime('2026-06-01 08:00:00', '%Y-%m-%d %H:%M:%S')
 to_day = datetime.now()
 
 days, hours, minutes, seconds = dhms_from_seconds(date_diff_in_seconds(data_cloud_summit_24_date,to_day))
@@ -35,10 +35,10 @@ with open("app.css") as f:
 with st.container():
     st.image("snowflake_summit_25.png")
     # st.title("Snowflake Summit 2025")
-    st.subheader("BUILD THE FUTURE TOGETHER WITH AI AND APPS")
-    # st.subheader("Thank You For Joining Snowflake Summit 24!")
-    # st.title("Save The Date For 2025")
-    st.write("MOSCONE CENTER | SAN FRANCISCO | JUNE 2-5, 2025")
+    # st.subheader("BUILD THE FUTURE TOGETHER WITH AI AND APPS")
+    st.subheader("Thank You For Joining Us At Snowflake Summit 25!")
+    st.subheader("Save The Date For 2026")
+    st.write("MOSCONE CENTER | SAN FRANCISCO | JUNE 1-4, 2026")
 
 st.header(f"{days} Days")
 
@@ -54,8 +54,8 @@ with st.container():
 st.markdown("___")
 spaces = "          "
 # st.caption(f"[Register Now](https://www.snowflake.com/summit/) // [Join Us at Dev Day](https://www.snowflake.com/summit/devday/)")
-st.caption(f"[Register Now](https://www.snowflake.com/summit/)")
-# st.caption(f"[Save The Date](https://www.snowflake.com/summit/save-the-date/)")
+# st.caption(f"[Register Now](https://www.snowflake.com/summit/)")
+st.subheader(f"[Save The Date](https://www.snowflake.com/en/summit/save-the-date/#form)")
 st.caption(f"Developed by [Dash](https://www.linkedin.com/in/dash-desai/) // Dedicated to [Saqib](https://www.linkedin.com/in/saqibmustafa/)")
 
 with st.container():
